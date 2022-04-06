3b22ce398a046c4a62b1fc1dbea1e7930d878e56
Manuel José Tornero <manueljosetornero@hotmail.com>
2022-04-06 09:49:50 +0200

    part 1

--- src/es/manu/Main.java
+++ src/es/manu/Main.java
@@ -7,20 +7,72 @@ public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         int tamano;
+        char[] tabla;
 
         System.out.println("Dime el tamaño del buscaminas [5 a 20]: ");
         tamano = entrada.nextInt();
 
-        crearTablero(tamano);
+        tabla = crearTablero(tamano);
+
+        System.out.println(tabla);
 
     }
 
-    private static void crearTablero (int tamano){
-        for (int i = 0; i < tamano; i++){
+    private static char[] crearTablero (int tamano) {
+        char[] tablero = new char[tamano];
+        int bombas;
+        for (int i = 0; i < tamano; i++) {
+            bombas = (int) (Math.random() * 2);
+            if (bombas != 0) {
+                tablero[i] += 0;
+            } else {
+                tablero[i] += '*';
+            }
+        }
+
+        for (int i = 0; i < tamano; i++) {
+            int contador = 0;
+            if (tablero[i] != '*') {
+                if (tablero[i - 1] == '*' && i != 0) {
+                    contador++;
+                }
+                if (tablero[i + 1] == '*' && i != tamano - 1) {
+                    contador++;
+                }
+                tablero[i] = (char) contador;
+            }
+        }
+        return tablero;
+    }
+
+    /*private static void  (int tamano){
+        for (int i = 0; i < tamano; i++) {
+            for (int j = 0; j < 4; j ++) {
+                if ()
+                System.out.print("+");
+            }
+        }*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*for (int i = 0; i < tamano; i++){
             for (int j = 0; j < 4; j++) {
                 for (int k = 0; k < 6; k++) {
                     if (k == 0 || k == 5) {
-                        System.out.print("+");
+                        System.out.print("|");
+                    } else if (j == 0 || (i == tamano - 1 && j == 3)) {
+                        System.out.print("--");
                     } else {
                         System.out.print("  ");
                     }
@@ -28,5 +80,5 @@ public class Main {
                 System.out.println();
             }
         }
-    }
+    }*/
 }
